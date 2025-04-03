8238989d98d3db97ca2290bb46662452c8ee275c
Amith B V <amithrao0606@gmail.com>
2025-04-03 17:18:37 +0530

    50% of home page completed.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#FFD6BA]`}
       >
         <div className="container mx-auto p-12">{children}</div>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="font-bold text-4xl pb-2 block border-b-2 border-gray-600">Home</h1>
 
-      <div className="flex items-center justify-between">
-        <h1>Snippets</h1>
+      <div className="flex items-center justify-between mt-4">
+        <h1 className="text-xl font-semibold">Snippets</h1>
 
-        <Button>New</Button>
+        <Link href={"/snippet/new"}>
+          <Button className="cursor-pointer">Add New Snippet</Button>
+        </Link>
       </div>
     </div>
   );
